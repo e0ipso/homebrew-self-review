559004bc277b1c5ae35c47de84837ae3b95d1f30
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2026-07-08 15:35:46 +0200

    Fix tap CI audits

--- Casks/self-review.rb
+++ Casks/self-review.rb
@@ -2,8 +2,7 @@ cask "self-review" do
   version "1.37.1"
   sha256 "c7a64a01f0c937627d1d8a58fe5c625fb071cd89eb8ddab74ccaaa3f7109d542"
 
-  url "https://github.com/e0ipso/self-review/releases/download/v#{version}/Self.Review-darwin-arm64-#{version}.zip",
-      verified: "github.com/e0ipso/self-review/"
+  url "https://github.com/e0ipso/self-review/releases/download/v#{version}/Self.Review-darwin-arm64-#{version}.zip"
   name "Self Review"
   desc "GitHub-style PR review UI for local git diffs"
   homepage "https://github.com/e0ipso/self-review"
